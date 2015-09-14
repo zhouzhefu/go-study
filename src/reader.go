a55a96e58a44761d66b1471f9d24cd99f1e91e4e
Jeff Zhou <zhou.zhefu@gmail.com>
2015-09-14 22:09:10 +0800

    add reader

--- src/reader.go
+++ src/reader.go
@@ -7,5 +7,15 @@ import (
 )
 
 func main() {
+	r := strings.NewReader("Hello, Reader!") //use string as example
+	b := make([]byte, 8)
 	
+	for { //while(true)
+		n, err := r.Read(b) //"n" is the number of bytes read
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if (err == io.EOF) { //catch (IoEOFException err) {...}
+			break
+		}
+	}
 }
